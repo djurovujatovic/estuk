b328e08fc9ff4307a44328c5eba3d7292b801b21
Djuro Vujatovic <djuro.vujatovic@gmail.com>
2015-03-16 00:40:52 -0500

    Added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ActiveRecord::Base
   		friendly_id :name, use: :slugged
   		
 		belongs_to :user
+		has_many :sales
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316042505) do
+ActiveRecord::Schema.define(version: 20150316053511) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20150316042505) do
   end
 
   add_index "books", ["slug"], name: "index_books_on_slug", unique: true
+
+  create_table "sales", force: :cascade do |t|
+    t.string   "buyer_email"
+    t.string   "seller_email"
+    t.integer  "amount"
+    t.string   "guid"
+    t.integer  "book_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
